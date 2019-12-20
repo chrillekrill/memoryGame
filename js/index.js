4f1177de299d9b19553c4157f793b02b608f6e56
chrillekrill <henricson21@hotmail.com>
2019-12-20 12:28:35 +0100

    pls help do not work

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ let playArea = document.getElementById("play-area");
 let message = document.getElementById("message");
 let score = document.getElementById("score");
 let failrate = document.getElementById("failrate");
-let resetButton = document.querySelector("reset-button");
+let resetButton = document.getElementById("reset-button");
+let cardPickAmmount = 2;
 let cardQuery;
 
 
@@ -12,23 +13,19 @@ checkWin = 0;
 
 arrayOfCards = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10'];
 
-cardsShuffled = [];
-
-emptyArray = []
-
-
 function resetBoard() {
-    // for(let i = 0; i < cardQuery.length; i++) {
-  
-        
-    // }
+    resetButton.disabled = true;
     while(playArea.firstChild) {
         playArea.removeChild(playArea.firstChild);
     }
     setTimeout(() => {
         setupBoard(shuffleCards(arrayOfCards));
+        handleClick();
+        resetButton.disabled = false;
     },1000)
+
 }
+    resetButton.addEventListener("click", resetBoard);
 
 function shuffleCards(cards) {
     for (var i = cards.length - 1; i > 0; i--) {
@@ -49,13 +46,14 @@ function setupBoard(cards) {
         playArea.appendChild(temporary);
     }
     score.innerHTML = 0;
+    failrate.innerHTML = 1;
 }
 
 setupBoard(shuffleCards(arrayOfCards));
 
 function handleClick() {
-
-    let cardPickAmmount = 2;
+    let removeMessage = message.innerHTML;
+;
     let firstCardPick;
     let secondCardPick;
 
@@ -79,22 +77,40 @@ function handleClick() {
                     // console.log("success");
                     cardPickAmmount = 2;
                     score.innerHTML++;
-                    if(score === 10) {
+                    if(score.innerHTML == 10) {
                         message.className = "wonMessage";
                         message.innerHTML = "You won!"
+                        setTimeout(() => {
+                            message.innerHTML = removeMessage;
+                            cardPickAmmount = 2;
+                            resetButton.click();
+                        },2000)
+
                     }
                 } else if(firstCardPick.innerText != secondCardPick.innerText) {
+                    
                     // console.log("fail");
-                    let removeFailMessage = message.innerHTML;
-                    cardPickAmmount = 2;
-                    message.innerHTML = "FAIL";
-                    message.className = "failMessage";
-                    setTimeout(() => {
-                        firstCardPick.className = 'cardFlipped';
-                        secondCardPick.className = 'cardFlipped';
-                        message.innerHTML = removeFailMessage;
-                        message.className = '';
-                    },700)
+                    failrate.innerHTML--;
+                    if(failrate.innerHTML == 0){
+                        console.log('failed')
+                        message.innerHTML = "GAME OVER";
+                        message.className = "gameOver";
+                        setTimeout(() => {
+                            message.innerHTML = removeMessage;
+                            cardPickAmmount = 2;
+                            resetBoard();
+                        },2000)
+                    }else {
+                        cardPickAmmount = 2;
+                        message.innerHTML = "FAIL";
+                        message.className = "failMessage";
+                        setTimeout(() => {
+                            firstCardPick.className = 'cardFlipped';
+                            secondCardPick.className = 'cardFlipped';
+                            message.innerHTML = removeMessage;
+                            message.className = '';
+                        },700)
+                    }
                 }
             }
         })

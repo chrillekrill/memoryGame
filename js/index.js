5083da46d0e264f2cb090f083602385808903c03
chrillekrill <henricson21@hotmail.com>
2019-12-22 17:47:30 +0100

    äntligen klart jfc

--- js/index.js
+++ js/index.js
@@ -3,13 +3,8 @@ let message = document.getElementById("message");
 let score = document.getElementById("score");
 let failrate = document.getElementById("failrate");
 let resetButton = document.getElementById("reset-button");
-let cardPickAmmount = 2;
-let cardQuery;
-
-
-failRate = 10;
-
-checkWin = 0;
+let removeMessage = message.innerHTML;
+let cardPickAmmount;
 
 arrayOfCards = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10'];
 
@@ -45,76 +40,87 @@ function setupBoard(cards) {
         temporary.innerHTML = cards[i];
         playArea.appendChild(temporary);
     }
+    cardPickAmmount = 2;
     score.innerHTML = 0;
-    failrate.innerHTML = 1;
+    failrate.innerHTML = 20;
+    handleClick();
 }
 
 setupBoard(shuffleCards(arrayOfCards));
 
-function handleClick() {
-    let removeMessage = message.innerHTML;
-;
-    let firstCardPick;
-    let secondCardPick;
+function checkWin() {
+    message.className = "wonMessage";
+    message.innerHTML = "You won!"
+    setTimeout(() => {
+        message.innerHTML = removeMessage;
+        cardPickAmmount = 2;
+        resetBoard();
+    },2000)
+}
 
-    cardQuery = playArea.querySelectorAll(".cardFlipped");
+function checkLoss() {
+    console.log('failed')
+    message.innerHTML = "GAME OVER";
+    message.className = "gameOver";
+    setTimeout(() => {
+        message.innerHTML = removeMessage;
+        cardPickAmmount = 2;
+        resetBoard();
+    },2000)
+}
 
-    for(let i = 0; i < cardQuery.length; i++) {
-        cardQuery[i].addEventListener("click", (event) => {
-            if(cardPickAmmount === 2) {
-                firstCardPick = event.target;
-                // console.log('First Picked Card: ' + firstCardPick.innerText)
-                event.target.className = 'card';
-                cardPickAmmount--;
-                // console.log('Flipped Cards: '+ cardPickAmmount)
-            } else if(cardPickAmmount === 1) {
-                secondCardPick = event.target;
-                // console.log('Second Picked Card: ' + secondCardPick.innerText)
-                event.target.className = 'card';
-                cardPickAmmount--;
-                // console.log('Flipped Cards: '+ cardPickAmmount)
-                if(firstCardPick.innerText === secondCardPick.innerText) {
-                    // console.log("success");
-                    cardPickAmmount = 2;
-                    score.innerHTML++;
-                    if(score.innerHTML == 10) {
-                        message.className = "wonMessage";
-                        message.innerHTML = "You won!"
-                        setTimeout(() => {
-                            message.innerHTML = removeMessage;
-                            cardPickAmmount = 2;
-                            resetButton.click();
-                        },2000)
-
-                    }
-                } else if(firstCardPick.innerText != secondCardPick.innerText) {
-                    
-                    // console.log("fail");
-                    failrate.innerHTML--;
-                    if(failrate.innerHTML == 0){
-                        console.log('failed')
-                        message.innerHTML = "GAME OVER";
-                        message.className = "gameOver";
-                        setTimeout(() => {
-                            message.innerHTML = removeMessage;
-                            cardPickAmmount = 2;
-                            resetBoard();
-                        },2000)
-                    }else {
-                        cardPickAmmount = 2;
-                        message.innerHTML = "FAIL";
-                        message.className = "failMessage";
-                        setTimeout(() => {
-                            firstCardPick.className = 'cardFlipped';
-                            secondCardPick.className = 'cardFlipped';
-                            message.innerHTML = removeMessage;
-                            message.className = '';
-                        },700)
-                    }
-                }
-            }
-        })
+function checkCards(cardOne, cardTwo){
+    if(cardOne.innerHTML === cardTwo.innerHTML) {
+        score.innerHTML++;
+        if(score.innerHTML > 1) {
+            checkWin();
+        } else {
+            cardPickAmmount = 2;
+        }
+    } else if(cardOne.innerHTML != cardTwo.innerHTML){
+        failrate.innerHTML--;
+        if(failrate.innerHTML < 1) {
+            checkLoss();
+        } else {
+            message.innerHTML = "FAIL";
+            message.className = "failMessage";
+            setTimeout(() => {
+                cardOne.className = 'cardFlipped';
+                cardTwo.className = 'cardFlipped';
+                cardPickAmmount = 2;
+                message.innerHTML = removeMessage;
+                message.className = '';
+            },700)
+        }
     }
 }
 
-handleClick();
+function handleClick() {
+    let cardQuery = Array.from(playArea.querySelectorAll(".cardFlipped"));
+    let firstCardPick;
+    let secondCardPick;
+
+    console.log(cardPickAmmount)
+
+    cardQuery.forEach(card => {
+        card.addEventListener("click", () => {
+                if(cardPickAmmount === 2 && card.className === 'cardFlipped') {
+                    firstCardPick = card;
+                    card.className = 'card';
+                    cardPickAmmount--;
+                    console.log(cardPickAmmount)
+                } else if(cardPickAmmount === 1  && card.className === 'cardFlipped') {
+                    secondCardPick = card;
+                    card.className = 'card';
+                    cardPickAmmount--;
+                    checkCards(firstCardPick, secondCardPick);
+                }
+        })
+    })
+}
+
+
+
+
+
+
